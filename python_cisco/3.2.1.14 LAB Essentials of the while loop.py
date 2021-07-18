b5966c45df2fb33de8cd129e86708a456414b6de
alvir218 <allan199215@gmail.com>
2021-07-17 22:21:21 -0300

    guias de codigo

---
+++ python_cisco/3.2.1.14 LAB Essentials of the while loop.py
@@ -0,0 +1,18 @@
+﻿blocks = int(input("Enter the number of blocks: "))
+
+#
+# Write your code here.
+#	
+
+height = 0
+total = 0
+
+#if blocks > 0:
+#    height = 1
+
+while total < blocks:
+    height = height + 1
+    ###total = blocks - heigth
+    total = total + height + 1
+
+print("The height of the pyramid:", height)
---
+++ python_cisco/3.2.1.15 LAB Collatz's hypothesis.py
@@ -0,0 +1,18 @@
+﻿c0 = int(input("digite um numero: "))
+steps = 0
+
+while c0 <= 0:
+    c0 = int(input("digite um numero: "))
+
+while c0 != 1:
+    if c0 % 2 == 0:
+        c0 = int(c0 / 2)
+    else:
+        c0 = int(3 * c0 + 1)
+    
+    print(c0)
+    
+    steps = steps + 1
+
+print("steps =", steps)
+
